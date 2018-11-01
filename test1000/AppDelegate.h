74b4886d76863953d8d77e2fee827b6fa53ca6fd
sunkang <958607177@qq.com>
2018-11-01 20:02:49 +0800

    Initial Commit

---
+++ test1000/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1000
+//
+//  Created by sunkang on 2018/11/1.
+//  Copyright © 2018年 sunkang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1000Tests/test1000Tests.m
@@ -0,0 +1,37 @@
+//
+//  test1000Tests.m
+//  test1000Tests
+//
+//  Created by sunkang on 2018/11/1.
+//  Copyright © 2018年 sunkang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test1000Tests : XCTestCase
+
+@end
+
+@implementation test1000Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
